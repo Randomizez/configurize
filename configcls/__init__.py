5cd256b542a2f0d38375e5ec0371498fcfa94515
zhouhy <henryzhou@step.ai>
2025-12-16 17:14:41 +0800

    add COnfigDiff

--- configcls/__init__.py
+++ configcls/__init__.py
@@ -1,2 +1,2 @@
 from .allowed_types import ALLOWED_TYPES, recur_to_allowed_types
-from .utils import Config, Ref, writable_property, DataClass, config_diff
+from .utils import Config, Ref, writable_property, DataClass, config_diff, ConfigDiff
